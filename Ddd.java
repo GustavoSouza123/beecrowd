1888e611aceee59ff775ee74464a4eec71dd39e3
Gustavo Souza <gustavoelia7@gmail.com>
2023-06-13 07:33:41 -0300

    finalized: DDD exercise

---
+++ Ddd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ddd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int D = sc.nextInt();
+        if(D == 61) {
+            System.out.println("Brasilia");
+        } else if(D == 71) {
+            System.out.println("Salvador");
+        } else if(D == 11) {
+            System.out.println("Sao Paulo");
+        } else if(D == 21) {
+            System.out.println("Rio de Janeiro");
+        } else if(D == 32) {
+            System.out.println("Juiz de Fora");
+        } else if(D == 19) {
+            System.out.println("Campinas");
+        } else if(D == 27) {
+            System.out.println("Vitoria");
+        } else if(D == 31) {
+            System.out.println("Belo Horizonte");
+        } else {
+            System.out.println("DDD nao cadastrado");
+        } 
+    }
+}
